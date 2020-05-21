46460a7b0dd6e72a670d1dd47066bd74902e410a
Ana Carolina <anabalan.b@gmail.com>
2020-05-21 15:22:09 -0300

    Criação dos componentes e front do index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Adicionado as rotas dos componentes criados
+  {path: '', component: IndexComponent},
+  {path: 'repository', component: RepositoryComponent},
+  {path: 'bitcoin', component: BitcoinComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    RepositoryComponent,
+    BitcoinComponent
   ],
   imports: [
     BrowserModule,
